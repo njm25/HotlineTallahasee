c9c8adf4bf1bd34de5ea398a64c30c3ff5e2b716
nicomilette <98992077+nicomilette@users.noreply.github.com>
2024-09-23 21:39:59 -0400

    more modifier

--- objects/player/Player.gd
+++ objects/player/Player.gd
@@ -14,18 +14,7 @@ func _ready():
 	player_inventory.create_weapon(toolgun, player)
 	player_inventory.create_weapon(testgun, player)
 	
-	# Separate the assignment of the modifier dictionaries
-	var weapon_add_mod = { "fire_rate": 0.2 }
-	var weapon_mult_mod = { "recoil_strength": 2 }
-	var weapon_modifiers = Modifier.new(weapon_add_mod, weapon_mult_mod)
 	
-	var player_add_mod = { "speed": 1000 }
-	var player_mult_mod = { "friction": 0.8 }
-	var player_modifiers = Modifier.new(player_add_mod, player_mult_mod)
-
-	# Apply to player inventory and controller
-	player_inventory.apply_modifier(weapon_modifiers)
-	player.apply_modifier(player_modifiers)
 	
 	pass
 
--- objects/world/StatMultiplierRegion.gd
+++ objects/world/StatMultiplierRegion.gd
@@ -11,14 +11,24 @@ func _ready():
 func _on_body_entered(other):
 	if other is PlayerController:
 		_player = other
-		_weapon = _player.get_node("PlayerInventory").current_weapon
-		prev_fire_rate = _weapon.fire_rate
-		_weapon.fire_rate = 0.1
+		var player_inventory = _player.get_node("PlayerInventory")
+		# Separate the assignment of the modifier dictionaries
+		var weapon_add_mod = { "fire_rate": 0.2 }
+		var weapon_mult_mod = { "recoil_strength": 2 }
+		var weapon_modifiers = Modifier.new(weapon_add_mod, weapon_mult_mod)
+		
+		var player_add_mod = { "speed": 1000 }
+		var player_mult_mod = { "friction": 0.8 }
+		var player_modifiers = Modifier.new(player_add_mod, player_mult_mod)
+
+		# Apply to player inventory and controller
+		player_inventory.apply_modifier(weapon_modifiers)
+		_player.apply_modifier(player_modifiers)
 		
 		
 func _on_body_exited(other):
 	if other == _player:
 		if other is PlayerController:
-			_weapon.fire_rate = prev_fire_rate  # Mark the player as no longer sliding
+			pass  # Mark the player as no longer sliding
 		_weapon = null
 		_player = null
