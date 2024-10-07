9a8948ef5d0b1ab8472eda6166cfc707e3a57b35
nicomilette <98992077+nicomilette@users.noreply.github.com>
2024-10-07 05:41:20 -0400

    Update Rooftop.gd

--- objects/world/Rooftop/Rooftop.gd
+++ objects/world/Rooftop/Rooftop.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+
+var player
+var player_is_dead: bool
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	connect("body_entered", self._on_body_entered)
@@ -11,14 +14,20 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body):
 	if body is PlayerController:
+		player = body
 		var roof = get_node("ColorRect")
-		# Create a tween and animate the modulate alpha (fade out)
+		if not player.is_connected("player_died", self._on_player_death):
+			player.connect("player_died", self._on_player_death)
 		var tween = create_tween()
 		tween.tween_property(roof, "modulate:a", 0.0, 0.2)  # Animate alpha to 0 over 1 second
 
+func _on_player_death():
+	player_is_dead = true
+
 func _on_body_exited(body):
-	if body is PlayerController:
-		var roof = get_node("ColorRect")
-		# Create a tween and animate the modulate alpha (fade in)
-		var tween = create_tween()
-		tween.tween_property(roof, "modulate:a", 1.0, 0.2)  # Animate alpha to 1 over 1 second
+	if !player_is_dead:
+		if body is PlayerController:
+			var roof = get_node("ColorRect")
+			# Create a tween and animate the modulate alpha (fade in)
+			var tween = create_tween()
+			tween.tween_property(roof, "modulate:a", 1.0, 0.2)  # Animate alpha to 1 over 1 second
